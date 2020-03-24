7f3889b8c1283c30f4d2aeb498826f0e65c1813a
kang.yang <kang.yang@b-and-qchina.com>
2020-03-25 00:15:32 +0800

    类

---
+++ java-demo/src/_0324_class/StaticInnerClassDemo.java
@@ -0,0 +1,25 @@
+package _0324_class;
+
+/**
+ * @ClassName StaticInnerClassDemo
+ * @Description 静态内部类
+ * @Author
+ * @Date 2020/3/24 23:39
+ * @Version 1.0
+ **/
+public class StaticInnerClassDemo {
+    public String name = "Python大星";
+    private static int age = 20;
+
+    static class Inner {
+        private String name;
+        public void fun() {
+            System.out.println("我是静态内部类");
+        }
+    }
+
+    public static void main(String[] args) {
+        StaticInnerClassDemo.Inner in = new StaticInnerClassDemo.Inner();
+        in.fun();
+    }
+}
