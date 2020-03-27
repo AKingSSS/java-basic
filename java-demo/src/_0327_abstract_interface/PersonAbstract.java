deb5eb6e53ae0cf994d3ad09c4173971b725c7d8
kang.yang <kang.yang@b-and-qchina.com>
2020-03-27 17:54:52 +0800

    抽象类

---
+++ java-demo/src/_0327_abstract_interface/PersonAbstract.java
@@ -0,0 +1,45 @@
+package _0327_abstract_interface;
+
+/**
+ * @ClassName PersonAbstract
+ * @Description
+ * @Author yangkang
+ * @Date 2020/3/27 16:47
+ * @Version 1.0
+ **/
+public abstract class PersonAbstract {
+    private String name;
+    private int age;
+
+    public PersonAbstract(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void say() {
+        System.out.println("我是" + getName() + ";今年" + getAge() + "岁");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+/*    public static void main(String[] args) {
+
+        PersonAbstract personAbstract =
+                new PersonAbstract("Python大星","18");
+        personAbstract.say();
+    }*/
+}
---
+++ java-demo/src/_0327_abstract_interface/SonAbstract.java
@@ -0,0 +1,42 @@
+package _0327_abstract_interface;
+
+/**
+ * @ClassName SonAbstract
+ * @Description
+ * @Author yangkang
+ * @Date 2020/3/27 17:15
+ * @Version 1.0
+ **/
+public class SonAbstract extends PersonAbstract {
+    private String hobby;
+    public SonAbstract(String name, int age,String hobby) {
+        super(name, age);
+        this.hobby = hobby;
+    }
+
+    @Override
+    public void say() {
+        System.out.println("我是" + getName() + ";今年" + getAge() + "岁。"
+                + "我的爱好是" + getHobby());
+    }
+
+    public void play() {
+        System.out.println("我的爱好是" + getHobby());
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public static void main(String[] args) {
+        PersonAbstract personAbstract =
+                new SonAbstract("Python大星",18,"打篮球");
+        personAbstract.say();
+        personAbstract = (SonAbstract) personAbstract;
+        ((SonAbstract) personAbstract).play();
+    }
+}
