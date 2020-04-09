c2d2692e82bf4f13e40202669eda032ccfa6e870
kang.yang <kang.yang@b-and-qchina.com>
2020-04-09 09:13:48 +0800

    变量不能被重写（覆盖），” 重写 “的概念只针对方法

--- java-demo/src/_0325_extend/mianshi03/Son.java
+++ java-demo/src/_0325_extend/mianshi03/Son.java
@@ -20,15 +20,22 @@ public class Son extends Parent {
     }
 
     public static void main(String[] args) {
+        /**
+         * 说明创建父类对象实现的时候用子类实现，此时这个对象的属性为父类的属性，不被子类覆盖。
+         * 但如果子类实现的时候调用重写的方法，则父类的方法会被覆盖。
+         * 总结：方法可重写，属性不可重写。父类的方法被子类覆盖，父类的属性不被子类覆盖。
+         *
+         * 变量不能被重写（覆盖），” 重写 “的概念只针对方法
+         */
         // 向上转型
         Parent p = new Son();
         // 调用的是 Son 的 play
         p.play();
         p.listen();
         System.out.println(p.i);
-        // 向下转型
+/*        // 向下转型
         p = (Son)p;
         ((Son) p).read();
-        System.out.println(p.i);
+        System.out.println(p.i);*/
     }
 }
